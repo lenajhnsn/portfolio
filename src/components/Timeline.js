722ffe47983f30392604cf1002d6cef8f6d4a7d3
Keir Johnson <keirjohnson@KJ-MbAir.local>
2024-10-27 10:08:58 -0600

    updated text of timeline

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -6,7 +6,7 @@ import './Timeline.css';  // Import the CSS file with the timeline styles
 const timelineData = [
     {
         title: 'Foundations in Psychology',
-        description: 'My background in psychology helped me understand the user. Through my studies, I learned what motivates behavior, drives adoption, and shapes experiences. This foundation makes me highly attuned to user needs and helps me design software that connects with people.',
+        description: 'My background in psychology helped me understand the user. I learned what motivates behavior, drives adoption, and shapes experiences. This foundation makes me attuned to user needs and helps me design software that connects with people.',
     },
     {
         title: 'Mastering Experimental Design',
@@ -21,12 +21,12 @@ const timelineData = [
         description: 'My experience in ed tech immersed me in the world of software engineering, helping me support and guide aspiring developers. This role taught me what it takes to succeed in the field, from essential skills to mindsets, and gave me years of experience talking tech.',
       },
       {
-        title: 'Full Stack Foundations at Tech Elevator',
+        title: 'Full Stack Development at Tech Elevator',
         description: 'At Tech Elevator, I learned Java, JavaScript, and full-stack development, bridging my theoretical knowledge with practical coding experience. I created full-stack applications, strengthened my understanding of the development lifecycle, and built a strong technical foundation.',
       },
       {
         title: 'Creating the Memo Archive',
-        description: 'My latest project, the Memo Archive, brings everything together. This full-stack application showcases my coding skills, problem-solving, and design sensibilities. Through it, I demonstrate the software engineering and user experience skills I’ve developed over the years.',
+        description: 'My latest project, the Memo Archive, brings everything together. This full-stack application showcases my coding skills, problem-solving, and design abilities.',
       }
 ];
 
