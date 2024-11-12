848c141546f61cb9c012ee4c26bff641653ddc7b
Keir Johnson <keirjohnson@KJ-MbAir.local>
2024-11-11 21:05:04 -0700

    added global styling and font downloads

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,10 @@ import './NavBar.css';
 const NavBar = () => {
     return (
         <nav className = "navbar">
+            {/* Portfolio title on the left side */}
+            <div className="navbar-title">Lena Johnson's Portfolio</div>
+            
+            {/* Navigation links */}
             <ul>
                 <li><a href = "home">Home</a></li>
                 <li><a href = "#timeline">About</a></li>
