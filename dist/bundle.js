ecad59e05331e89f1e6b338c43744dba15141708
Lena Johnson <lena.johnson000@gmail.com>
2024-11-22 12:44:09 -0700

    fixed issue with style.css

--- dist/bundle.js
+++ dist/bundle.js
@@ -754,7 +754,7 @@ eval("module.exports = __webpack_require__.p + \"assets/vuelogo.f1a0c00e8cf57263
 /******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
-/******/ 		__webpack_require__.p = "/dist/";
+/******/ 		__webpack_require__.p = "/portfolio/";
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/jsonp chunk loading */
